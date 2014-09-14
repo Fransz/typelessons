c86dfbb2a2e320bea4cfe4710bb97663266a4b0a
Frans <fjaspers@xs4all.nl>
2014-09-14 12:17:35 +0200

    Weights are shown correct in the taskdetail

--- src/views/TaskDetailView.coffee
+++ src/views/TaskDetailView.coffee
@@ -9,13 +9,19 @@ App.TaskDetailView = Backbone.View.extend
         @$el.show()
         @render()
 
+    # displays the details of a task
+    # Weights are displayed in 3 decimals without the leading zero.
+    #
+    # @return void
     render: () ->
-        letters = _.filter(@model.get("letters"), (l) -> l isnt ' ')
+        letters = @model.get "letters"
+        weights = _.clone(@model.get "weights")
+        weights = weights.map(((w) -> w.toFixed(3)[1 ..]))
         # score = @model.get("exams").cummScore()
 
         # s = score.time % 60
         # m = Math.floor(score.time / 60)
         # score.time = "#{if(m < 10) then '0' + m else m}:#{if(s < 10) then '0' + s else s}"
 
-        @$(".letters").html @lettertemplate letters: letters
+        @$(".letters").html @lettertemplate letters: letters, weights: weights
         return @
--- test/integration/taskdetailTest.js
+++ test/integration/taskdetailTest.js
@@ -62,22 +62,23 @@ describe("The task detail panel", function () {
     });
 
     describe("#when opened", function(done) {
-        it("should show 8 letters", function (done) {
+        it("should show 8 letters, and a space", function (done) {
+            /* scrape the weights text from the detail view, compare these with the scraped weights from the taskview*/
+
             /* jshint -W083, -W030 */
             var detailLetters;
 
             function detailTest(text) {
+                    text = text.replace("letters", "");
                     text = text.replace(/\n/g, "");
                     detailLetters = text;
-                    expect(detailLetters).to.have.length(8);
+                    expect(detailLetters).to.have.length(9);
             }
-
             function detailText(cb) {
                 return function(keyRows) {
                     keyRows[0].getText().then(cb);
                 };
             }
-
             function detailKeyRows(cb) {
                 return function (taskDetails) {
                     taskDetails[0].findElements(webdriver.By.className("keyrow")).then(cb);
@@ -86,7 +87,6 @@ describe("The task detail panel", function () {
 
             function taskTest(text) {
                 text = text.replace(/\n/g, "");
-                console.log(text);
                 for(var i = 0, l = text.length; i < l; i++) {
                         expect(detailLetters).to.contain(text[i]);
                 }
@@ -115,34 +115,23 @@ describe("The task detail panel", function () {
                             driver.findElements(webdriver.By.id("8lettertasks")).then(
                                 taskTasks(taskKeyRows(taskText(taskTest)))
                     );
-
-                /* Get the letters from the task; there shouldbe the same 8. */
-                // driver.findElements(webdriver.By.id("8lettertasks")).then(function(elms) {
-                    // elms[0].findElements(webdriver.By.className("task")).then(function(tasks) {
-                        // tasks[0].findElements(webdriver.By.className("keyrow")).then(function(keyrows) {
-                            // keyrows[0].getText().then(function(text) {
-
-                                // text = text.replace(/\n/g, "");
-                            // console.log(text);
-                                // for(var i = 0, l = text.length; i < l; i++) {
-                                    // expect(detailLetters).to.contain(text[i]);
-                                // }
-
-                            // });
-                        // });
-                    // });
-                // });
             }).then(done);
         });
 
-        it.skip("should show 8 weights", function (done) {
-            /* jshint -W083, -W030 */
-            driver.findElements(webdriver.By.id("taskdetail")).then(function(elms) {
-                var detail = elms[0];
-                detail.findElements(webdriver.By.className("weights")).then(function(letters) {
-                    expect(letters).to.have.length(8);
-                });
-            }).then(done);
+        it("should show 9 weights", function (done) {
+            function detailTest(weights) {
+                expect(weights).to.have.length(9);
+            }
+
+            function detailWeights(cb) {
+                return function (details) {
+                    details[0].findElements(webdriver.By.className("weight")).then(cb);
+                };
+            }
+
+            driver.findElements(webdriver.By.id("taskdetail")).then(
+                    detailWeights(detailTest)
+                ).then(done);
         });
 
         it.skip("should show the number of tries", function (done) {
