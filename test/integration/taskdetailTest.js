40000a1d34ba53a8b473ae47f74a16b6d3bea688
Frans <fjaspers@xs4all.nl>
2014-09-14 21:28:34 +0200

    Cancel button with its integration test

--- test/integration/taskdetailTest.js
+++ test/integration/taskdetailTest.js
@@ -276,5 +276,15 @@ describe("The task detail panel", function () {
                 });
             }).then(done);
         });
+
+        it("should have a cancel button", function (done) {
+            driver.findElements(webdriver.By.css("#taskdetail .buttons #taskdetailcancelbutton")).then(function (buttons) {
+                expect(buttons).to.have.length(1);
+                buttons[0].getText().then(function(text) {
+                    // @TODO test click behaviour
+                    expect(text).to.be.equal("cancel");
+                });
+            }).then(done);
+        });
     });
 });
